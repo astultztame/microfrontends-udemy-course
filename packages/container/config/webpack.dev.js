91022bca5d38190f7582b328974784bc4566c5fd
astultztame <alst@tame.events>
2021-08-06 13:24:45 +0200

    Initial commit

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,35 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const PORT = 8080;
+const MARKETING_PORT = 8081;
+const AUTH_PORT = 8082;
+const DASHBOARD_PORT = 8083;
+
+const devConfig = {
+    mode: 'development',
+    output: {
+        publicPath: `http://localhost:${PORT}/`
+    },
+    devServer: {
+        port: PORT,
+        historyApiFallback: {
+            index: 'index.html'
+        }
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {
+                marketing: `marketing@http://localhost:${MARKETING_PORT}/remoteEntry.js`,
+                auth: `auth@http://localhost:${AUTH_PORT}/remoteEntry.js`,
+                dashboard: `dashboard@http://localhost:${DASHBOARD_PORT}/remoteEntry.js`
+            },
+            shared: packageJson.dependencies
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, devConfig);
